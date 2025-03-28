cbdbd7613f8f456bfe50528d6f09e28b0ebf1bc4
R7di4 <R7di4am@gmail.com>
2025-03-28 19:53:13 +0330

    i dont know

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<?php ?>
+<!-- Footer -->
+<footer class="text-center mt-5">
+    <div class="container">
+        <p>
+            <a href="#" class="me-2">Privacy Policy</a> |
+            <a href="#">Terms of Service</a>
+        </p>
+    </div>
+</footer>
+
+<script src="./assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,28 @@
+<?php ?>
+<!-- Navbar -->
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Reprogram Blog</title>
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">Reprogram.ir</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="login.php">Register | Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
